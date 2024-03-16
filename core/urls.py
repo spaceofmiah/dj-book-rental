124e556eb3b64e7f2cd768a25bf8cbdd31aa83ca
spaceofmiah <agbonzeosazuwa@gmail.com>
2024-03-16 01:24:06 +0100

    setup: htmx and request processing

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,38 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect
 from django.contrib import admin
 from django.views import generic
-from django.urls import path
+from django.urls import path, reverse
+from django.contrib import messages
+import requests 
+
+
+def search(request:HttpRequest):
+    try:
+        response = requests.get(f"https://openlibrary.org/search.json?title={request.GET.get('book')}")
+        data = response.json()['docs'][0]
+        title = data['title']
+        author_name = data['author_name'][0]
+        pages = data['number_of_pages_median']
+    except:
+        author_name = None
+        pages = None
+        title = None
+
+    return render(request, 'search.html', {'author_name': author_name, 'pages': pages, 'title':title})
+
+def rent(request:HttpRequest):
+    book_name = request.POST.get('book_name')
+    student_name = request.POST.get('student')
+    return_date = request.POST.get('return_date')
+    print(book_name, student_name, return_date)
+    return HttpResponseRedirect(reverse('index'))
+
 
 urlpatterns = [
     path('dashboard/', generic.TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
     path('', generic.TemplateView.as_view(template_name='index.html'), name='index'),
+    path('search/', search, name='search'),
+    path('rent/', rent, name='rent'),
     path('admin/', admin.site.urls),
 ]
